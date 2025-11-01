87e25f46096a5cf2ded257e79e24d8aa01de22bf
RintaDev <93689788+RintaDev5792@users.noreply.github.com>
2025-11-01 11:00:45 -0400

    did stuff
    alll kindsa stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -5,7 +5,15 @@ import androidx.annotation.NonNull;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 
 import java.lang.reflect.Array;
 @TeleOp(name="Cybirds-Tele-1")
@@ -17,11 +25,13 @@ public class TeleOpMode extends OpMode
     DcMotor leftFront;
     DcMotor rightSlide;
     DcMotor leftSlide;
-
+    DcMotor intake;
+    Limelight3A limelight;
+    int slide_up_pos = 2350;
+    int slide_down_pos = 0;
+    int slide_target_pos = 100;
     double speed;
-
     double speed_fine_inc = 0.05;
-
     boolean r_bump_1 = false;
     boolean l_bump_1 = false;
 
@@ -34,13 +44,34 @@ public class TeleOpMode extends OpMode
         leftFront = hardwareMap.dcMotor.get("frontleft");
         rightSlide = hardwareMap.dcMotor.get("rightslide");
         leftSlide = hardwareMap.dcMotor.get("leftslide");
+        intake = hardwareMap.dcMotor.get("intake");
         leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //set slides to target their current position
+        slide_target_pos = 0;
+        //initialise slide encoder doodads :3
+        leftSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightSlide.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftSlide.setTargetPosition(slide_target_pos);
+        rightSlide.setTargetPosition(slide_target_pos);
+        leftSlide.setPower(1.0);
+        rightSlide.setPower(1.0);
+        leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+        //initialise limelight on pipeline 0
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(0);
+        limelight.start();
+        //set drive speed at 0.5 initially
         speed = 0.5;
+        //initialise bumpers as "not pressed"
         r_bump_1=false;
         l_bump_1=false;
     }
@@ -63,24 +94,13 @@ public class TeleOpMode extends OpMode
         FR   -   -   -
         FL   -   +   -
         */
+
+        //don't ask how this works, it is wonderful
         rightRear.setPower(-speed*(gamepad1.left_stick_x-gamepad1.left_stick_y+gamepad1.right_stick_x));
         leftRear.setPower(-speed*(gamepad1.left_stick_x+gamepad1.left_stick_y+gamepad1.right_stick_x));
         rightFront.setPower(-speed*(-gamepad1.left_stick_x-gamepad1.left_stick_y+gamepad1.right_stick_x));
         leftFront.setPower(-speed*(-gamepad1.left_stick_x+gamepad1.left_stick_y+gamepad1.right_stick_x));
     }
-    public void drive(double speed) {
-        /*TABLE OF INP
-             LX  LY  RX
-        RR   +   -   -
-        RL   +   +   -
-        FR   -   -   -
-        FL   -   +   -
-        */
-    rightRear.setPower(-speed);
-    leftRear.setPower(speed);
-    rightFront.setPower(-speed);
-    leftFront.setPower(speed);
-}
 
     public void p1_fine_speed_control() {
         if (gamepad1.right_bumper) {
@@ -102,25 +122,75 @@ public class TeleOpMode extends OpMode
     }
 
     public void slide_control() {
+        if (gamepad1.a) {
+            slide_target_pos = slide_up_pos;
+        }
+        if (gamepad1.b) {
+            slide_target_pos = slide_down_pos;
+        }
         if (gamepad1.dpad_down) {
-            leftSlide.setPower(1);
-            rightSlide.setPower(-1);
+            slide_target_pos -= 25;
         }
         if (gamepad1.dpad_up) {
-            leftSlide.setPower(-1);
-            rightSlide.setPower(1);
+            slide_target_pos += 25;
+            /*if (slide_target_pos > 10000) {
+                slide_target_pos = 0;
+            }*/
         }
         if (gamepad1.dpad_left) {
-            leftSlide.setPower(0);
-            rightSlide.setPower(0);
+            slide_target_pos = 0;
+
+            leftSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+        if (leftSlide.isBusy() || slide_target_pos > 50) {
+            leftSlide.setPower(1.0);
+        } else {
+            leftSlide.setPower(0.1);
+        }
+        if (rightSlide.isBusy() || slide_target_pos > 50) {
+            rightSlide.setPower(1.0);
+        } else {
+            rightSlide.setPower(0.1);
+        }
+        leftSlide.setTargetPosition(slide_target_pos);
+        rightSlide.setTargetPosition(slide_target_pos);
+
+    }
+
+    public void intake_control() {
+        if (gamepad1.right_trigger > 0.2) {
+            intake.setPower(-1.0);
+        } else {
+            intake.setPower(0.0);
         }
     }
 
+    public void limelight_things() {
+        if (gamepad1.left_trigger > 0.2) {
+            return;
+        }
+        LLResult result = limelight.getLatestResult();
+        if (result != null) {
+            if (result.isValid()) {
+                Pose3D botpose = result.getBotpose();
+                telemetry.addData("tx", result.getTx());
+                telemetry.addData("ty", result.getTy());
+                telemetry.addData("Botpose", botpose.toString());
+            }
+        }
+    }
 
     public void do_p1_things() {
         p1_fine_speed_control();
         dual_joy_control();
         slide_control();
+        intake_control();
+        intake_control();
+        limelight_things();
     }
 
     public void do_p2_things() {
@@ -131,7 +201,11 @@ public class TeleOpMode extends OpMode
     public void loop() {
         do_p1_things();
         do_p2_things();
-        telemetry.addData("running?", true);
+        telemetry.addData("> current slide target", slide_target_pos);
+        telemetry.addData("left slide position", leftSlide.getCurrentPosition());
+        telemetry.addData("right slide position", rightSlide.getCurrentPosition());
+        telemetry.addData("> left slide busy", leftSlide.isBusy());
+        telemetry.addData("> right slide busy", rightSlide.isBusy());
         telemetry.update();
     }
 }
